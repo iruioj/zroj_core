faa2395b84c484639027663fdfd8a03ba501aa86
PlanarG <0x131cc05@gmail.com>
2023-03-06 21:31:38 +0800

    feat: adjust problem structure

--- problem/src/lib.rs
+++ problem/src/lib.rs
@@ -1,7 +1,14 @@
 mod data;
 mod config;
 mod problem_set;
-mod statement;
-mod target_oj_type;
+mod problem_type;
 mod accumulate_rule;
 mod task;
+
+pub use data::Problem;
+pub use config::ProblemConfig;
+pub use problem_type::ProblemType;
+pub use accumulate_rule::AccumulateRule;
+pub use task::SubTask;
+pub use task::TestCase;
+pub use task::Tasks;
--- problem/src/statement.rs
+++
@@ -1,6 +0,0 @@
-pub struct Statement {
-    title: String, 
-    content: String, 
-    /// 出题人提供的官方题解
-    solution: Option<String>
-}
