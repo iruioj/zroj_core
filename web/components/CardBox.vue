5869c1f4365da59887824fd93fb137d5ae607ddd
sshwy <jy.cat@qq.com>
2023-04-05 13:52:31 +0800

    feat(web): index page: add annoucement section

---
+++ web/components/CardBox.vue
@@ -0,0 +1,14 @@
+<!-- 卡片布局 -->
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div class="border border-black/20 rounded">
+    <div class="p-2 border-b border-black/20">
+      <slot name="header"></slot>
+    </div>
+    <div class="p-2">
+      <slot name="body"></slot>
+    </div>
+  </div>
+</template>
--- web/components/NavButton.vue
+++ web/components/NavButton.vue
@@ -1,19 +1,17 @@
 <script setup lang="ts">
-import type { Component } from 'vue'
 
 defineProps<{
   to: string
-  icon: Component
 }>()
 </script>
 
 <template>
   <NuxtLink class="
-      hover:bg-brand hover:bg-opacity-10
-      h-full px-2 py-2 
-      transition duration-200 ease-in-out
-      " :to="to">
-    <component :is="icon" class="inline w-7 h-7 pr-1" />
+        hover:text-brand
+        inline-block h-full p-2
+        text-sm
+        transition duration-200 ease-in-out
+        " :to="to">
     <slot></slot>
   </NuxtLink>
 </template>
