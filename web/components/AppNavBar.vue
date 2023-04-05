b33207351888ad56cde37bd28b09c05739be72ec
sshwy <jy.cat@qq.com>
2023-04-05 13:52:31 +0800

    feat(web): index page: add annoucement section

--- web/components/AppNavBar.vue
+++ web/components/AppNavBar.vue
@@ -1,25 +1,21 @@
 <script setup lang="ts">
 import NavButton from './NavButton.vue'
-import {
-  HomeIcon,
-  AboutIcon,
-  ListIcon,
-  QuizIcon,
-  AssignmentIcon,
-} from './icons'
+
 </script>
 
 <template>
-  <header class="
-    sticky top-0 z-30 bg-white
-    flex items-center border-b border-slate-900/10 dark:border-slate-50/[0.06]">
+  <header class="sticky top-0 z-30 bg-white
+        flex items-center 
+        border-b border-slate-900/10 dark:border-slate-50/[0.06]">
     <NuxtLink to="/">
       <div class="inline-block px-4 font-bold text-xl text-brand">ZROJ</div>
     </NuxtLink>
-    <!-- <NavButton to="/" :icon="HomeIcon">Home</NavButton> -->
-    <NavButton to="/problem/1" :icon="ListIcon">Problems</NavButton>
-    <!-- <NavButton to="/" :icon="QuizIcon">Contests</NavButton> -->
-    <NavButton to="/submission/2" :icon="AssignmentIcon">Submissions</NavButton>
-    <NavButton to="/oneoff" :icon="AboutIcon">Custom Test</NavButton>
+    <div class="flex">
+      <!-- <NavButton to="/" :icon="HomeIcon">Home</NavButton> -->
+      <NavButton to="/problem/1">Problems</NavButton>
+      <!-- <NavButton to="/" :icon="QuizIcon">Contests</NavButton> -->
+      <NavButton to="/submission/2">Submissions</NavButton>
+      <NavButton to="/oneoff">Custom Test</NavButton>
+    </div>
   </header>
 </template>
---
+++ web/components/SectionContainer.vue
@@ -0,0 +1,18 @@
+<!-- SectionContainer 用于显示一个段落，类似没有边框的 card -->
+<script lang="ts" setup>
+defineProps<{ title: string }>()
+</script>
+
+<template>
+  <div class="">
+    <div class="px-1 py-2 
+      border-b-4 border-b-brand border-double 
+      text-brand font-bold text-lg
+      ">
+      {{ title }}
+    </div>
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
