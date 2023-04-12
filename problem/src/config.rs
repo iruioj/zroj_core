3744284d03ad572b6ced41fb4c8a7f37f2b2e772
PlanarG <0x131cc05@gmail.com>
2023-04-12 10:02:23 +0800

    feat: update problem

--- problem/src/config.rs
+++ problem/src/config.rs
@@ -1,17 +1,24 @@
-use std::path::{PathBuf};
+use std::{path::{PathBuf}, fs::File};
 use serde_derive::{Serialize, Deserialize};
+use zip::ZipArchive;
 
 use crate::{task::Tasks, accumulate_rule::AccumulateRule};
 
-/// 题目配置文件需要包含的信息
+/// 题目配置文件需要包含的信息，T 表示测试数据的配对方式
 #[derive(Serialize, Deserialize)]
-pub struct ProblemConfig<T> {
+pub struct ProblemConfig<T: Checkable> {
+    /// 测试数据
     pub tasks: Tasks<T>,
-    pub checker_path: PathBuf, 
-    pub validator_path: PathBuf, 
-    pub hacker_path: PathBuf,
+    /// checker 文件相对路径
+    pub checker: PathBuf, 
+    pub validator: Option<PathBuf>, 
+    pub hacker: Option<PathBuf>,
     /// 记分规则  
     pub rule: AccumulateRule, 
-    pub time_limit: Option<i32>, 
-    pub memory_limit: Option<i32>, 
+    pub time_limit: Option<u32>, 
+    pub memory_limit: Option<u32>, 
+}
+
+pub trait Checkable {
+    fn check(&self, zip: &mut ZipArchive<&File>) -> bool;
 }
--- problem/src/lib.rs
+++ problem/src/lib.rs
@@ -1,13 +1,11 @@
-mod data;
 mod config;
 mod problem_set;
-mod problem_type;
+mod problem_detail;
 mod accumulate_rule;
 mod task;
 
-pub use data::Problem;
 pub use config::ProblemConfig;
-pub use problem_type::ProblemType;
+pub use problem_detail::ProblemDetail;
 pub use accumulate_rule::AccumulateRule;
 pub use task::SubTask;
 pub use task::TestCase;
