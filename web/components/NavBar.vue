70e6d1677827a3dc168a9e2163dd2e0d4510cf59
sshwy <jy.cat@qq.com>
2023-04-13 12:13:26 +0800

    feat(web): responsive layout for navbar

--- web/components/NavBar.vue
+++ web/components/NavBar.vue
@@ -3,23 +3,29 @@ import NavButton from "./NavButton.vue";
 </script>
 
 <template>
-  <header
-    class="sticky top-0 z-30 bg-white flex items-center border-b border-slate-900/10 dark:border-slate-50/[0.06]"
-  >
-    <NuxtLink to="/">
-      <div class="inline-block px-4 font-bold text-xl text-brand">ZROJ</div>
-    </NuxtLink>
-    <div class="flex print:hidden">
+  <header class="sticky top-0 z-30 border-b border-slate-900/10 dark:border-slate-50/[0.06]">
+    <div class="flex items-center bg-white">
+      <NuxtLink to="/">
+        <div class="inline-block px-4 font-bold text-xl text-brand">ZROJ</div>
+      </NuxtLink>
+      <div class="print:hidden hidden sm:flex">
+        <NavButton to="/problemset">Problems</NavButton>
+        <!-- <NavButton to="/" :icon="QuizIcon">Contests</NavButton> -->
+        <NavButton to="/submission/2">Submissions</NavButton>
+        <NavButton to="/oneoff">Custom Test</NavButton>
+      </div>
+      <div class="grow"></div>
+      <div>
+        <div class="py-2 px-4 print:hidden">
+          <TextLink to="/auth/signin">Sign In/Up</TextLink>
+        </div>
+      </div>
+    </div>
+    <div class="mx-2 print:hidden sm:hidden bg-white border-t border-slate-900/10 dark:border-slate-50/[0.06]">
       <NavButton to="/problemset">Problems</NavButton>
       <!-- <NavButton to="/" :icon="QuizIcon">Contests</NavButton> -->
       <NavButton to="/submission/2">Submissions</NavButton>
       <NavButton to="/oneoff">Custom Test</NavButton>
-    </div>
-    <div class="grow"></div>
-    <div>
-      <div class="py-2 px-4 print:hidden">
-        <TextLink to="/auth/signin">Sign In/Up</TextLink>
-      </div>
     </div>
   </header>
 </template>
