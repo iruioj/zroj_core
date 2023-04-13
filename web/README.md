e887630b919a2b13220d548baadd928c83fed901
sshwy <jy.cat@qq.com>
2023-04-13 10:41:42 +0800

    feat(web): add favicon

--- web/README.md
+++ web/README.md
@@ -1,3 +1,10 @@
 # ZROJ web
 
 https://dev.to/tao/adding-eslint-and-prettier-to-nuxt-3-2023-5bg
+
+开发环境：
+
+```
+yarn: 3.4.1
+node: 18.16.0
+```
--- web/components/UBtn.vue
+++ web/components/UBtn.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="px-2 py-1 text-slate-800 border rounded border-slate-400 hover:text-brand hover:border-brand focus:ring-1 ring-brand transition-all ring-offset-1"
+    class="px-2 py-1 text-slate-800 border rounded border-slate-400 hover:text-brand hover:border-brand focus:ring-1 ring-brand transition-all ring-offset-1 select-none"
   >
     <slot></slot>
   </button>
