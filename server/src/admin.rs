8cb5acf6ecb7543f87a2112f44a3a91c63983aa2
compilationfail <2946316707@qq.com>
2023-04-17 22:54:31 +0800

    fixes

--- server/src/admin.rs
+++ server/src/admin.rs
@@ -1,16 +1,5 @@
-use actix_web::{get, web, HttpResponse, Responder};
-
-#[get("/global_config")]
-async fn global_config() -> impl Responder {
-    HttpResponse::Ok().body(format!("Server configuration"))
-}
-#[get("/judge_actions")]
-async fn judge_actions() -> impl Responder {
-    HttpResponse::Ok().body(format!("Watch judge list"))
-}
+use actix_web::{web};
 
 pub fn service() -> actix_web::Scope {
     web::scope("/admin")
-        .service(global_config)
-        .service(judge_actions)
 }
--- server/src/data.rs
+++ server/src/data.rs
@@ -1,3 +1,6 @@
 pub mod user;
 pub mod schema;
+#[cfg(not(feature="mysql"))]
 pub type UserDataManagerType = user::hashmap::HashMap;
+#[cfg(feature="mysql")]
+pub type UserDataManagerType = user::database::Database;
