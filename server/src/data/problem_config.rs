6fe689a54c83c50b2767ba33ee01e43e71480b2e
compilationfail <2946316707@qq.com>
2023-05-05 01:08:21 +0800

    Add ProblemConfigManager

--- server/src/data/problem_config.rs
+++ server/src/data/problem_config.rs
@@ -2,37 +2,45 @@
 //! for example, problem access and create date
 
 pub type AManager = dyn Manager + Sync + Send;
+use super::error::{Error, Result};
+use super::schema::ProblemAccess;
+use crate::data::group::AManager as GroupAManager;
+use crate::{data::schema::ProblemConfig, ProblemID, UserID};
 use async_trait::async_trait;
 pub use hashmap::FsManager;
 use std::sync::Arc;
-use crate::{ProblemID, data::schema::ProblemConfig};
 
 #[async_trait]
 pub trait Manager {
+    async fn get(&self, pid: ProblemID, uid: UserID) -> Result<ProblemConfig>;
+    async fn get_access(&self, pid: ProblemID, uid: UserID) -> Result<ProblemAccess>;
     fn to_amanager(self) -> Arc<AManager>;
 }
 
 mod hashmap {
-    use serde::{Serialize, Deserialize};
+
+    use crate::data::schema::UserGroup;
 
     use super::*;
+    use serde::{Deserialize, Serialize};
     use std::{collections::HashMap, path::PathBuf, sync::RwLock};
 
     #[derive(Serialize, Deserialize)]
     struct Data(HashMap<ProblemID, ProblemConfig>);
 
     /// 文件系统存储信息
-    #[derive(Serialize, Deserialize)]
     pub struct FsManager {
         data: RwLock<Data>,
+        groups: Arc<GroupAManager>,
         path: PathBuf,
     }
 
     impl FsManager {
-        pub fn new(path: PathBuf) -> Self {
+        pub fn new(path: PathBuf, groups: Arc<GroupAManager>) -> Self {
             let mut r = Self::load(&path).unwrap_or(Data(HashMap::new()));
             Self {
                 data: RwLock::new(r),
+                groups,
                 path: path.clone(),
             }
         }
@@ -51,12 +59,43 @@ mod hashmap {
                 self.path.display()
             ));
         }
+        fn get(&self, pid: ProblemID) -> Result<ProblemConfig> {
+            let guard = self.data.read()?;
+            Ok((*guard).0.get(&pid).ok_or(Error::InvalidArgument(format!(
+                "Problem {} does not exist",
+                pid)
+            ))?.clone())
+        }
     }
     #[async_trait]
     impl super::Manager for FsManager {
         /// consume self and return its Arc.
         fn to_amanager(self) -> Arc<AManager> {
             Arc::new(self)
+        }
+        async fn get(&self, pid: ProblemID, uid: UserID) -> Result<ProblemConfig> {
+            let value = self.get(pid)?;
+            if value.owner != uid {
+                Err(Error::Forbidden("Only owner can access config".to_string()))
+            } else {
+                Ok(value.clone())
+            }
+        }
+        async fn get_access(&self, pid: ProblemID, uid: UserID) -> Result<ProblemAccess> {
+            let mut t: ProblemAccess = ProblemAccess::None;
+            let value = self.get(pid)?;
+            for (ug, a) in value.access {
+                let flag = match ug {
+                    UserGroup::User(id) => id.clone() == uid,
+                    UserGroup::Group(gid) => {
+                        self.groups.group_contains(gid.clone(), uid).await?
+                    }
+                };
+                if flag && a.clone() > t {
+                    t = a.clone()
+                }
+            }
+            Ok(t)
         }
     }
 }
