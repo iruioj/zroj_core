776d694df924a2eb6fbd2bf648568caf0eafe1fa
blunt-axe <blunt_axe@163.com>
2023-05-09 14:01:05 +0800

    Added CacheCE (for Real)

--- judger/src/basic.rs
+++ judger/src/basic.rs
@@ -1,33 +1,24 @@
 //! 传统题的评测
 
-use std::path::PathBuf;
+use std::{path::PathBuf};
 
-use crate::{
-	lang::LangOption,
-	JudgeResult,
-};
+use problem::Builtin as Problem;
 
-pub use problem::Builtin;
+use crate::{lang::LangOption, JudgeResult, Cache, error::Error};
 
 pub struct Submission<L: LangOption> {
-	/// 提交记录对应的题目
-	prob: Builtin,
-	/// 语言
+	prob: Problem,
 	lang: L,
-	/// 源代码相对路径
 	source: PathBuf,
 }
 
 impl<L: LangOption> Submission<L> {
-    pub fn new(prob: Builtin, lang: L, source: PathBuf) -> Self {
-        Self {
-			prob,
-			lang,
-            source,
-        }
+    pub fn new(prob: Problem, lang: L, source: PathBuf) -> Self {
+        return Self { prob, lang, source };
     }
-	pub fn judge(&self) -> JudgeResult {
-		eprintln!("{}", self.source.display());
-		todo!()
+	/// 评测需传入缓存系统的可变引用
+	pub fn judge_traditional(&self, cache: &mut Cache) -> Result<JudgeResult, Error> {
+		let _exec = cache.get_exec(&self.lang, &self.source);
+		todo!();
 	}
 }
